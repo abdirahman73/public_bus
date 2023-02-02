ab78cd486bc3932c018e4082b75d3615aa022458
Abdirahman Tusbahle <119303709+abdirahman73@users.noreply.github.com>
2023-02-02 06:20:48 -0800

    Update routes.js

--- public_bus/public/js/routes.js
+++ public_bus/public/js/routes.js
@@ -1,9 +1,26 @@
  async function getRoutes(){
     try {
-        var response = await fetch('http://localhost:3000/api/buses', {method: 'GET'})
+      const response = await fetch('http://localhost:3000/api/buses', {
+        method: 'GET'
+      })
+
         const allRoutes = await response.json();
       const data =  document.getElementById('data')
-        
+      for(let route of allRoutes){
+        const row = document.createElement('tr')
+        row.innerHTML = `
+        <td> ${route.id} </td>
+        <td> ${route.bus_no} </td>
+        <td> ${route.from_city} </td>
+        <td> ${route.to_city} </td>
+        <td> ${route.date} </td>
+        <td> ${route.time} </td>
+        <td> ${route.price} </td>
+        <td> <a href='/booking?id=${route.id}'>Book now </a> </td>
+        `
+
+        data.append(row)
+      }
     } catch (error) {
         console.log(error)
     }
